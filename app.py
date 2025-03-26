105ab916e0a2b29a9a661e53228c2e158ba9e221
Noel Triguero Torres <notrito@users.noreply.huggingface.co>
2025-03-26 09:49:13 +0000

    Update app.py

--- app.py
+++ app.py
@@ -55,7 +55,7 @@ available_voices = voice_options.get(selected_lang, {})
 
 # Show second dropdown only if the first selection is valid
 if available_voices:
-    selected_voice = st.selectbox("Select a voice", list(available_voices.values()))  #VOICE
+    selected_voice = list(available_voices.values())  #VOICE
     name_voice = selected_voice = st.selectbox("Select a voice", list(available_voices.keys()))
     st.write(f"🔹 Selected Voice Code: `{selected_voice}`")
 
